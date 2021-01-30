63b21cc91e860567966bf92cfc7dd26e77910543
juannavascalvente <juan.navas.calvente@gmail.com>
2021-01-30 19:14:31 +0100

    Refactor process

--- inc/Receiver.h
+++ inc/Receiver.h
@@ -22,14 +22,53 @@ class Receiver {
     static char dat,buff[100],GGA_code[3];
     static bool isItGgaString;
     static unsigned int indexGga;
-    static bool is_GGA_received_completely;
+    static bool isGgaReceivedCompletely;
 
     static CoordinatesContainer coordinates;
+
+    /**
+     * @fn:         ReadData
+     * @brief       Reads GPS coordinate from GPS device
+     */
+    static void ReadData();
+
+    /**
+     * @fn:         isDataReceived
+     * @brief       Checks if a full coordinate has been received
+     * @return      true if a GPS coordinate has been received
+     *              false otherwise
+     */
+    static bool isDataReceived() {return isGgaReceivedCompletely; };
+
+    /**
+     * @fn:         setDataReceived
+     * @brief       Sets  isGgaReceivedCompletely value to input value
+     * @param value (IN) Value to set
+     */
+    static void setDataReceived(bool value) { isGgaReceivedCompletely = value; };
 public:
+    /**
+     * @fn:         Init
+     * @brief       Initializes receiver resources
+     * @return      true if initialized
+     *              false otherwise
+     */
     static bool Init();
 
+    /**
+     * @fn:         Process
+     * @brief       Receives GPS coordinates and saves them in coordinates container
+     * @return      true if process finished while running
+     *              false otherwise
+     */
     static bool Process();
 
+    /**
+     * @fn:         IsRunning
+     * @brief       Checks if receiver is in RUNNING process
+     * @return      true if receiver is in RUNNING process
+     *              false otherwise
+     */
     static bool IsRunning();
 };
 
